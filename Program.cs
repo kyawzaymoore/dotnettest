05ef979821e7c81f0a82246c5d93835f0ecef763
Kyaw Zay Moore <kyawzaymoore@gmail.com>
2021-08-15 20:49:16 +0630

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
